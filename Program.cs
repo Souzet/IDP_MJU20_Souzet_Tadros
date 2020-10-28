7db51b03c47de5a2c7d6eb7a8df8093af238aa21
souzet tadros <souzettadros@gmail.com>
2020-10-28 19:02:30 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace IDP_MJU20_Souzet_Tadros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal minId;
+            int numberId;
+            int yearId;
+            int dayId;
+            int monthID;
+            int jan;
+            //Ask user for socia security nummber
+            Console.WriteLine();
+            Console.WriteLine(" änge ditt personnummer: ");
+            minId =Convert.ToDecimal(Console.ReadLine());
+            //Validate the input
+            int[] nuM = new int[12];
+            nuM[0,1,2,3]= 
+            for (yearId = 1753; yearId<=2020; yearId++)
+            {
+                for (dayId = 1; dayId<= 31; dayId++)
+                {
+                    int[] monthId = new int[12];
+                    monthId[0]= for (jan = 0; jan<=31 jan++)
+
+            }
+
+                }
+                
+                Console.WriteLine(" Personnummer måste vara 12 siffror");
+            }
+  
+            else
+            {
+                if (minId % 2 == 0)
+                {
+                    Console.WriteLine("Personnummer är till man");
+                }
+                else
+                {
+                    Console.WriteLine("Personnummer är till kvinna");
+
+                }
+
+            }   
+            
+                
+        }
+    }
+}
