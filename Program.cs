2545867b93300f691f71184fbc44c8a20ed91b80
souzet tadros <souzettadros@gmail.com>
2020-10-30 16:17:58 +0100

    Ändra

--- Program.cs
+++ Program.cs
@@ -6,48 +6,86 @@ namespace IDP_MJU20_Souzet_Tadros
     {
         static void Main(string[] args)
         {
-            decimal minId;
-            int numberId;
-            int yearId;
-            int dayId;
-            int monthID;
-            int jan;
-            //Ask user for socia security nummber
-            Console.WriteLine();
-            Console.WriteLine(" änge ditt personnummer: ");
-            minId =Convert.ToDecimal(Console.ReadLine());
-            //Validate the input
-            int[] nuM = new int[12];
-            nuM[0,1,2,3]= 
-            for (yearId = 1753; yearId<=2020; yearId++)
+            string perNr;
+            Console.WriteLine("Ange ditt 12 siffriga personnummer: ");
+            perNr = Console.ReadLine();
+            if (perNr.Length != 12)
             {
-                for (dayId = 1; dayId<= 31; dayId++)
-                {
-                    int[] monthId = new int[12];
-                    monthId[0]= for (jan = 0; jan<=31 jan++)
-
+                Console.WriteLine("Personnummer måste vara 12 siffror");
             }
-
-                }
-                
-                Console.WriteLine(" Personnummer måste vara 12 siffror");
-            }
-  
             else
             {
-                if (minId % 2 == 0)
+                Console.WriteLine(" Rätt antal personnummer");
+                string yearID = perNr.Substring(0, 4);
+                int year = int.Parse(yearID);
+                if (year < 1753 || year > 2020)
                 {
-                    Console.WriteLine("Personnummer är till man");
+                    Console.WriteLine("fel årtalet");
                 }
                 else
                 {
-                    Console.WriteLine("Personnummer är till kvinna");
+                    Console.WriteLine("Rätt årtalet");
+                    string monthID = perNr.Substring(4, 2);
+                    int month = int.Parse(monthID);
+                    if(month <1 || month > 12)
+                    {
+                        Console.WriteLine(" Fel månadtalet");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Rätt månadtalet");
+                        string dayID = perNr.Substring(6, 2);
+                        int day = int.Parse(dayID);
+                        if ((day < 1 && day > 31) && (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12))
+                        {
+                            Console.WriteLine(" Felaktig dag");
+                        }                            
+                        else if ((day < 1 || day > 30) && (month == 4 || month == 6 || month == 9 || month == 11))
+                        {
+                            Console.WriteLine(" Felaktig dag");
+                        }                           
+                        else if ((day < 1 || day > 28) && (month == 2))
+                        {
+                            Console.WriteLine(" Felaktig dag");
+                        }                            
+                        else if (day == 29 && month == 2 && (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)))
+                        {
 
+                        }
+
+                        else
+                        {
+                            Console.WriteLine(" Rätt dag");
+                            string nnN = perNr.Substring(8, 3);
+                            int nNN = int.Parse(nnN);
+                            if (nNN < 000 || nNN > 999)
+                            {
+                                Console.WriteLine(" Fel personnummer");
+                            }
+                            else
+                            {
+                                Console.WriteLine(" Rätt personnummer");
+                                string cNum = perNr.Substring(11, 1);
+                                int numC = int.Parse(cNum);
+                                if (numC % 2 == 0)
+                                {
+                                    Console.WriteLine("Personnummer är till man");
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Personnummer är till kvinna");
+                                }
+
+                            }
+
+                                
+
+
+                        }
+
+                    }
                 }
-
-            }   
-            
-                
+        }
         }
     }
 }
